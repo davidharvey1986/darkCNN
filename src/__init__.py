55efb694d4db569bc3e3546718fdab9e0f25199f
David Harvey <davidharvey1986@googlemail.com>
2021-11-16 10:59:04 +0100

    updates to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,7 @@
 from .main import *
-
+from .mainModel import *
+from .augmentData import *
+from .getSIDMdatay import *
+from .globalVariablesy import *
+from .inceptionModules import *
+from .tools import *
