2f1c8587b709fdd76df08cb29b2234ba590af9a0
David Harvey <davidharvey1986@googlemail.com>
2021-11-16 14:29:53 +0100

    version added with pkg

--- src/__init__.py
+++ src/__init__.py
@@ -5,4 +5,6 @@ from .getSIDMdata import *
 from .globalVariables import *
 from .inceptionModules import *
 from .tools import *
+import pkg_resources  # part of setuptools                                                                                        
+
 __version__ = pkg_resources.require("darkCNN")[0].version
